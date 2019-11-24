ad39e7fd51a20e6888595d562edd8bc81c899d0a
fercarozzi <carozzi@ualberta.ca>
2019-11-23 23:30:58 -0700

    fix?

--- docs/make.jl
+++ docs/make.jl
@@ -8,14 +8,8 @@ makedocs(
 	doctest = false,
  	clean = true,
 	checkdocs = :all,
-<<<<<<< HEAD
-=======
-	assets = ["assets/logo.png"]
->>>>>>> parent of d04e9e3... update logo
 	sitename = "SeisMain.jl",
-	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing)== "true",
-		assets = ["assets/logo.png"],
-		),
+	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing)== "true"),
 	pages = [
 	       "Home" => "index.md",
 	       "Manual" => Any[
